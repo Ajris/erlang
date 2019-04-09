1af8fbf4929e224d86441fa495423dcd6a6db879
Ajris <hubiflower@gmail.com>
2019-04-09 03:29:49 +0200

    it works wowow

--- pollution/src/pollution.erl
+++ pollution/src/pollution.erl
@@ -14,7 +14,7 @@
 -export([createMonitor/0]).
 -export([addStation/3]).
 -export([addValue/5]).
-%%-export([removeValue/4]).
+-export([removeValue/4]).
 %%-export([getOneValue/4]).
 %%-export([getStationMean/3]).
 %%-export([getDailyMean/3]).
@@ -34,11 +34,11 @@ test() ->
   P5 = addValue({4, 5}, calendar:local_time(), pm10, "Wartosc", P4),
   P6 = addValue({4, 5}, calendar:local_time(), pm25, "Wartosc", P5),
   P7 = addValue({4, 5}, calendar:local_time(), temp, "Wartosc", P6),
-  P8 = addValue({4, 5}, calendar:local_time(), temp, "Wartosc", P7),
+  P8 = addValue({3, 4}, calendar:local_time(), temp, "Wartosc", P7),
   P9 = addValue("Stacja4", calendar:local_time(), pm10, "Wartosc", P8),
-  LastStation = lists:last(P9#monitor.stations),
-  lists:flatlength(LastStation#station.measurements),
-  P9
+  P10 = removeValue({4,5}, calendar:local_time(), pm10, P9),
+  LastStation = lists:last(P10#monitor.stations),
+  lists:flatlength(LastStation#station.measurements)
 .
 
 createMonitor() ->
@@ -69,16 +69,26 @@ addValue({X, Y}, Date, Type, Value, Monitor) ->
     [CurrentStation] ->
       case isGoodType(Type) of
         true ->
-          Measurement = #measurement{station = CurrentStation, date = Date, type = Type, value = Value},
-          ActualizedMeasurements = CurrentStation#station.measurements ++ [Measurement],
+          Measurements = CurrentStation#station.measurements,
+          ResultMeasurement = lists:filter(fun(Measurement) -> (Measurement#measurement.date == Date) and (Measurement#measurement.type == Type) end, Measurements),
+          case ResultMeasurement of
+            [] ->
+              Measurement = #measurement{station = CurrentStation, date = Date, type = Type, value = Value},
+              ActualizedMeasurements = CurrentStation#station.measurements ++ [Measurement],
+              NewCurrentStation = CurrentStation#station{
+                measurements = ActualizedMeasurements
+              },
+              ActualizedStation = lists:delete(CurrentStation, Monitor#monitor.stations) ++ [NewCurrentStation],
+              Monitor#monitor{
+                stations = ActualizedStation
+              };
 
-          NewCurrentStation = CurrentStation#station{
-            measurements = ActualizedMeasurements
-          },
-          ActualizedStation = lists:delete(CurrentStation, Monitor#monitor.stations) ++ [NewCurrentStation],
-          Monitor#monitor{
-            stations = ActualizedStation
-          };
+            [CurrentMeasurement | Tail] ->
+              erlang:error("Something went wrong");
+            [_] -> erlang:error("Something went wrong")
+          end;
+
+
         false ->
           erlang:error("Wrong type")
       end
@@ -109,7 +119,75 @@ addValue(Name, Date, Type, Value, Monitor) ->
   end
 .
 
+removeValue({X, Y}, Date, Type, Monitor) ->
+  Stations = Monitor#monitor.stations,
+  ResultStation = lists:filter(fun(Station) -> (Station#station.coords == {X, Y}) end, Stations),
+  case ResultStation of
+    [] ->
+      erlang:error("Coudln't find that station with provided X and Y");
+    [CurrentStation] ->
+      case isGoodType(Type) of
+        true ->
+          Measurements = CurrentStation#station.measurements,
+          [ResultMeasurementHead | Tail] = lists:filter(fun(Measurement) -> (Measurement#measurement.date == Date) and (Measurement#measurement.type == Type) end, Measurements),
+          ResultMeasurement = [ResultMeasurementHead],
+          case ResultMeasurement of
+            [] ->
+              erlang:error("No measurement found");
+            [CurrentMeasurement] ->
+              ActualizedMeasurements = lists:delete(CurrentMeasurement, CurrentStation#station.measurements),
+
+              NewCurrentStation = CurrentStation#station{
+                measurements = ActualizedMeasurements
+              },
+              ActualizedStation = lists:delete(CurrentStation, Monitor#monitor.stations) ++ [NewCurrentStation],
+
+              Monitor#monitor{
+                stations = ActualizedStation
+              };
+            [_] -> erlang:error("Something went wrong")
+          end;
+        false ->
+          erlang:error("Wrong type")
+      end
+  end;
+
+removeValue(Name, Date, Type, Monitor) ->
+  Stations = Monitor#monitor.stations,
+  ResultStation = lists:filter(fun(Station) -> (Station#station.name == Name) end, Stations),
+  case ResultStation of
+    [] ->
+      erlang:error("Coudln't find that station with provided X and Y");
+    [CurrentStation] ->
+      case isGoodType(Type) of
+        true ->
+          Measurements = CurrentStation#station.measurements,
+          [ResultMeasurementHead | Tail] = lists:filter(fun(Measurement) -> (Measurement#measurement.date == Date) and (Measurement#measurement.type == Type) end, Measurements),
+          ResultMeasurement = [ResultMeasurementHead],
+          case ResultMeasurement of
+            [] ->
+              erlang:error("No measurement found");
+            [CurrentMeasurement] ->
+              ActualizedMeasurements = lists:delete(CurrentMeasurement, CurrentStation#station.measurements),
+
+              NewCurrentStation = CurrentStation#station{
+                measurements = ActualizedMeasurements
+              },
+              ActualizedStation = lists:delete(CurrentStation, Monitor#monitor.stations) ++ [NewCurrentStation],
+
+              Monitor#monitor{
+                stations = ActualizedStation
+              };
+            [_] -> erlang:error("Something went wrong")
+          end;
+        false ->
+          erlang:error("Wrong type")
+      end
+  end
+.
+
 isGoodType(pm10) -> true;
 isGoodType(pm25) -> true;
 isGoodType(temp) -> true;
 isGoodType(_) -> false.
+
