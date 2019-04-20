72192e7d620ae7d2a049edcae3e460d229d98550
Ajris <hubiflower@gmail.com>
2019-04-20 14:05:01 +0200

    Started pollution server

---
+++ pollution/src/pollution_server.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @author ajris
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. Apr 2019 14:04
+%%%-------------------------------------------------------------------
+-module(pollution_server).
+-author("ajris").
+
+%% API
+-export([start/0]).
+-export([stop/0]).
+
+start() ->
+  erlang:error(not_implemented).
+
+stop() ->
+  erlang:error(not_implemented).
+
+init() ->
+  erlang:error(not_implemented).
